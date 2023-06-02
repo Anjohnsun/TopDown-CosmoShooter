e9129b0495dde2b90a685cc3bfb7044b46d44370
dycc <likvidoff@gmail.com>
2023-06-02 10:02:09 +0300

    some changes

--- Assets/_Source/InteractableObjects/Container.cs
+++ Assets/_Source/InteractableObjects/Container.cs
@@ -23,7 +23,8 @@ public class Container : ADamagable
             }
         }
 
-        Instantiate(_deathParticle, transform.position, new Quaternion());
+        // particle
+        if (_deathParticle != null) Instantiate(_deathParticle, transform.position, new Quaternion());
 
         //dissolving
         Destroy(gameObject);
